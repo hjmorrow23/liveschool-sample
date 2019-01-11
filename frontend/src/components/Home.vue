1cd404bb6b530dfa75a50dade617acb95ba738d5
hjmorrow23 <hjmorrow23@gmail.com>
2019-01-11 08:46:51 -0500

    Initial commit

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <AppHeader></AppHeader>
+    </div>
+</template>
+<script>
+import AppHeader from '@/components/AppHeader.vue'
+
+export default { 
+    name: 'Home',
+    components: {
+        AppHeader
+    }
+}
+</script>
+<style>
+
+</style>
+  
